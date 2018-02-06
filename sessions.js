a7ab6466c87f38862e74a1cefa02dfd71b7330e7
tranthebao <tranthebaocntt31b@gmail.com>
2018-02-06 10:40:11 +0700

    Tutorial express finished learn cookie

---
+++ sessions.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var session = require('express-session');
+var cookieparser =  require('cookie-parser');
+
+var app = express();
+app.use(cookieparser());
+app.use(session({
+    secret: "Shh, its a secret!"
+}));
+app.get('/', function(req, res){
+    if(req.session.page_views){
+        req.session.page_views++;
+        res.send("view visited this page" + req.session.page_views + "times");
+    }else{
+        req.session.page_views = 1;
+        res.send("wellcome to this page for the first time !");
+    }
+});
+
+app.listen(8000);
